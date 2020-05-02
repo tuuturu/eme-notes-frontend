3e92086b2361cd6aead43634079a2ae9272e78d7
Julius Pedersen <deifyed@gmail.com>
2020-05-02 10:23:42 +0200

    ✅ Add navigation to /all

---
+++ src/components/IconMenu.vue
@@ -0,0 +1,28 @@
+<template>
+	<svg
+		width="24"
+		height="24"
+		viewBox="0 0 24 24"
+		fill="none"
+		xmlns="http://www.w3.org/2000/svg"
+		class="IconMenu"
+		v-on="$listeners"
+	>
+		<title>{{ alt }}</title>
+		<rect width="24" height="4" fill="currentColor" />
+		<rect y="10" width="24" height="4" fill="currentColor" />
+		<rect y="20" width="24" height="4" fill="currentColor" />
+	</svg>
+</template>
+
+<script>
+export default {
+	name: 'IconMenu',
+	props: {
+		alt: {
+			type: String,
+			default: () => ''
+		}
+	}
+}
+</script>
